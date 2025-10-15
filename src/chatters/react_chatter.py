218f1e68b5e6e2d6fbf66778d87942a63d56745c
MJ141592 <mjohnson31415926@gmail.com>
2025-10-15 15:31:34 +0100

    save fix maybe

--- src/chatters/react_chatter.py
+++ src/chatters/react_chatter.py
@@ -1,5 +1,5 @@
 from typing import List
-from src.schemas import WhatsAppMessage, ChatAction, ReactAction, Chatter
+from src.schemas import WhatsAppMessage, ChatAction, ReactAction, Chatter, Action
 
 from datetime import datetime, timedelta
 
@@ -7,5 +7,5 @@ class ReactChatter(Chatter):
     def __init__(self, emoji_name: str):
         self.emoji_name = emoji_name
 
-    def on_receive_messages(self, messages: List[WhatsAppMessage]) -> List[ChatAction]:
-        return [ReactAction(message=messages[0], timestamp=datetime.now(), emoji_name=self.emoji_name)]
+    def on_receive_messages(self, messages: List[WhatsAppMessage]) -> List[ReactAction]:
+        return [ReactAction(message_to_react=messages[0], timestamp=datetime.now(), emoji_name=self.emoji_name)]
