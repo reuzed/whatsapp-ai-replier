1538e6b4e80eb52aeefb1c901d0e5c4375c2d8a0
MJ141592 <mjohnson31415926@gmail.com>
2025-10-16 10:49:26 +0100

    save file from prev commit

--- src/chatters/react_chatter.py
+++ src/chatters/react_chatter.py
@@ -7,5 +7,5 @@ class ReactChatter(Chatter):
     def __init__(self, emoji_name: str):
         self.emoji_name = emoji_name
 
-    def on_receive_messages(self, messages: List[WhatsAppMessage]) -> List[ReactAction]:
+    def on_receive_messages(self, messages: List[WhatsAppMessage], chat_name: str) -> List[ReactAction]:
         return [ReactAction(message_to_react=messages[0], timestamp=datetime.now(), emoji_name=self.emoji_name)]
