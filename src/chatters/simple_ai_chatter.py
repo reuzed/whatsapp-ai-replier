861bdb544ae96eb673c62cdcb5193e9d3bd29ea5
reuzed <49994279+reuzed@users.noreply.github.com>
2025-10-18 15:59:16 +0100

    break siple ai chatter

--- src/chatters/simple_ai_chatter.py
+++ src/chatters/simple_ai_chatter.py
@@ -7,6 +7,13 @@ class SimpleAIChatter(Chatter):
     def __init__(self):
         self.llm_manager = LLMManager()
 
-    def on_receive_messages(self, messages: List[WhatsAppMessage], chat_name: str) -> List[ChatAction]:
-        response_message = self.llm_manager.generate_whatsapp_chatter_response(messages)
-        return [ChatAction(message=response_message, timestamp=datetime.now())]
+    async def on_receive_messages(self, messages: List[WhatsAppMessage], chat_name: str) -> List[ChatAction]:
+        response_message = await self.llm_manager.generate_whatsapp_chatter_response(messages)
+        # TODO fix
+        # message = WhatsAppMessage(
+        #     sender="Me",
+        #     content=message,
+            
+        # )
+        # return [ChatAction(message=response_message, timestamp=datetime.now())]
+        return []
